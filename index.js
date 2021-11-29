2f4b57de72ca06b7c7ecd6db598824ade9cab3c1
Kaiserlucas <32715218+Kaiserlucas@users.noreply.github.com>
2021-11-29 09:10:42 +0100

    Added Express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const port = process.env.PORT || 3000
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send({headers: req.headers})
+})
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
