7ad02e986410ae4bd9832434a0c80a4e5c74c75a
Kaiserlucas <32715218+Kaiserlucas@users.noreply.github.com>
2021-12-10 19:21:46 +0100

    Changed cookies settings for testing

--- index.ts
+++ index.ts
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(function (req, res, next) {
 
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://travel-site-project.netlify.app/');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
