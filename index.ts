86346429a8f9f12968dd267ae2c89bd43ff28cbd
Kaiserlucas <32715218+Kaiserlucas@users.noreply.github.com>
2021-12-10 16:10:46 +0100

    Slightly changed CORS header

--- index.ts
+++ index.ts
@@ -63,7 +63,7 @@ app.use(
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://travel-site-project.netlify.app/');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
