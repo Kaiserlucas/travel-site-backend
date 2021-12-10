05e44ac98f477b5d3c126e626971973e76885eff
Kaiserlucas <32715218+Kaiserlucas@users.noreply.github.com>
2021-12-10 18:03:06 +0100

    Changed cors rules

--- index.ts
+++ index.ts
@@ -18,6 +18,7 @@ const authService = new AuthService();
 app.use(express.json());
 app.use(cookieParser());
 
+/*
 app.use(
   OpenApiValidator.middleware({
     apiSpec: "./openapi.yaml",
@@ -25,6 +26,7 @@ app.use(
     validateResponses: false,
   })
 );
+ */
 
 const checkLogin = async (
   req: Request,
