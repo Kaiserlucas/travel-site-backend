31e89a0b9cf9b5450f768b0499974fc07d21f82a
Kaiserlucas <32715218+Kaiserlucas@users.noreply.github.com>
2021-12-11 18:33:53 +0100

    Attempt to send cookies

--- index.ts
+++ index.ts
@@ -110,8 +110,9 @@ app.post("/login", async (req, res) => {
     maxAge: 60 * 60 * 1000,
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-  }).send;
+  });
   console.log(sessionId);
+  res.json({ status: "ok" });
 });
 
 app.post("/signup", async (req, res) => {
